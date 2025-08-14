d4ffc47364b313fc3f49447eb9aad98c8c06c8e5
kolci <kolci@duck.com>
2025-08-14 16:59:48 +0200

    Renew the website

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 # app.py on Render - The Viewer
 import os
 import psycopg2
-from flask import Flask, render_template, jsonify 
+from datetime import datetime
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 DATABASE_URL = os.getenv("DATABASE_URL")
@@ -10,42 +11,46 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 def home():
     return render_template('index.html')
 
-def get_latest_articles():
-    """Helper function to fetch the 50 most recent processed articles."""
+def articles():
+    '''
+    Returns articles as JSON for a specific date in the query
+    '''
+
+    date_str = request.args.get('date')
+    target_date = None
+
+    if date_str:
+        try:
+            target_date = datetime.strptime(date_str, '%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({"Invalid date format, use YYYY-MM-DD"}),400
+    else:
+        target_date = datetime.now(timezone.utc).date()
     articles = []
     try:
         with psycopg2.connect(DATABASE_URL) as conn:
             with conn.cursor() as cur:
-                cur.execute(
-                    """
+                query = """
                     SELECT content, subject_company, sentiment, scraped_at
                     FROM briefs 
                     WHERE sentiment IS NOT NULL 
-                    ORDER BY scraped_at DESC
-                    LIMIT 50;
-                    """
-                )
-                articles = cur.fetchall()
+                      AND CAST(scraped_at AT TIME ZONE 'UTC' AS DATE) = %s
+                    ORDER BY scraped_at DESC;
+                """
+                cur.execute(query,(target_date))
+                articles_db = cur.fetchall()
     except Exception as e:
-        print(f"Database query failed: {e}")
-    return articles
-
-
-@app.route('/api/articles')
-def api_articles():
-    """Returns the latest articles as JSON."""
-    db_articles = get_latest_articles() 
-    
-    articles_as_dicts = []
-    for article in db_articles:
-        articles_as_dicts.append({
-            'content': article[0],
-            'company': article[1],
-            'sentiment': article[2],
-            'time': article[3].strftime('%Y-%m-%d %H:%M') + ' UTC' if article[3] else 'N/A'
+        print(f"Fetch failed: {e}")
+        return jsonify({"Failed to retrieve articles"}), 500
+    articles_dict = []
+    for article in articles_db:
+        articles_dict.append({
+            'content':article[0],
+            'company':article[1],
+            'sentiment':article[2],
+            'time': article[3].strftime('%Y-%m-%d %H:%M') + 'UTC' if article[3] else 'N/A'
         })
-        
-    return jsonify(articles_as_dicts)
+    return jsonify(articles_dict)
 
 @app.route('/healthz')
 def health_check():
